2916bab7abe2bf29507ff8e473fda3eed1dd9603
dosqi <dosqion@gmail.com>
2013-04-02 02:57:17 +0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<header>
+<link rel="stylesheet" type="text/css" href="style.css">
+</header>
+<body>
+<center>
+<table id='mainframe'>
+  <tr>
+    <th><h1>Træningssystem</h1></th> <!-- Overskrift oeverst -->
+  </tr>
+  <tr id='innerEdge'>
+    <td>
+      <div id='content'> <!-- Hovedindholdet i midten -->
+        <div id='login'>
+          <form action='main.php' method='post'>
+            <input type='password' name='password'>
+            <input type='submit' value='Log ind'>
+          </form>
+        </div>
+      </div>
+    </td>
+    <td>
+    </td>
+  </tr>
+  <tr id='bottombar'>
+    <td>
+    </td>
+    <td>
+      Måløv Badminton Club
+    </td>
+  </tr>
+</table>
+</center>
+</body>
+</html>
